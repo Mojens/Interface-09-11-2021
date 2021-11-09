5331a1026d24f9e7f92878851d036265ad758ac3
Mojens <moha135h@stud.kea.dk>
2021-11-09 11:30:28 +0100

    Person Sort opgave

---
+++ src/PersonComparableSort/Person.java
@@ -0,0 +1,28 @@
+package PersonComparableSort;
+
+import java.util.Objects;
+
+public class Person implements Comparable{ //Man kan skrive således public class Person implements Comparable<Person>
+  //Så slipper vi for at lave en instance of længere ned i compareTo
+  Integer yearOfBirth;
+
+  public Person(Integer yearOfBirth){ //Konstruktør med int med tal af årstal for fødsel
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  @Override
+  public String toString() { //Laver en toString til min arrayList så den ikke kommer i koder
+    return ""+yearOfBirth;
+  }
+
+
+  public int compareTo(Object o) { //Denne metode sammenligner Person objekterne med hinanden med deres årstal
+    if (o instanceof Person){
+      if (yearOfBirth<((Person) o).yearOfBirth){ //Mindre end -1
+        return -1;
+      }else if (yearOfBirth>((Person) o).yearOfBirth){ //Større end 1
+        return 1;
+      }
+    }return 0; //Ligmed 0
+  }
+}
---
+++ src/PersonComparableSort/PersonMain.java
@@ -0,0 +1,28 @@
+package PersonComparableSort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PersonMain {
+  public static void main(String[] args) {
+//Laver 4 person objekter med forskellige fødelsår
+    Person person1 = new Person(2000);
+    Person person2 = new Person(2001);
+    Person person3 = new Person(1996);
+    Person person4 = new Person(2001);
+//Laver en ArrayList
+    ArrayList<Person> personBirthYear = new ArrayList();
+//Tilføjer alle person objekter ind i ArrayListen
+    personBirthYear.add(person1);
+    personBirthYear.add(person2);
+    personBirthYear.add(person3);
+    personBirthYear.add(person4);
+//Printer listen ud før den bliver sorteret
+    System.out.println("Before Sort");
+    System.out.println(personBirthYear);
+//Printer listen ud efter sorteret
+    System.out.println("After Sort");
+    Collections.sort(personBirthYear); //Denne her sorter min arrayList ud fra Laveste tal til det højeste
+    System.out.println(personBirthYear); //Den her printer den arrayList ud efter den er sorteret
+  }
+}
